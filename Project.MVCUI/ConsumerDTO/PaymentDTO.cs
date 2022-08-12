57a8f581007f48bcac1983bb474b08c54c4f3306
fatih daşdelen <fatihdasdelen@hotmail.com>
2022-08-12 16:01:57 +0300

    BLL katmanı tamamlandı

---
+++ Project.MVCUI/ConsumerDTO/PaymentDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project.MVCUI.ConsumerDTO
+{
+    public class PaymentDTO
+    {
+        public int ID { get; set; }
+        public string CardUserName { get; set; }
+        public string SecurityNumber { get; set; }
+        public string CardNumber { get; set; }
+        public int CartExpiryMonth { get; set; }
+        public int CartExpiryYear { get; set; }
+        public decimal ShoppingPrice { get; set; }
+
+
+    }
+}
